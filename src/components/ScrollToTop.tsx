f8ee412675431e72ec77afb98c19a761d761f17f
contactjeetsingh <74710141+contactjeetsingh@users.noreply.github.com>
2026-04-04 10:58:36 +0530

    Update ScrollToTop.tsx

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -2,14 +2,15 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, search, hash, key } = useLocation();
 
   useEffect(() => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth", // remove "smooth" if you want instant
+      left: 0,
+      behavior: "instant"
     });
-  }, [pathname]);
+  }, [pathname, search, hash, key]);
 
   return null;
 };
